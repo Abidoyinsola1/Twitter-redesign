ac5ed6b150096627e378e826e69125664691ae58
Abiola Doyinsola <ifeobayemi@gmail.com>
2020-11-17 10:57:34 +0100

    Change UI and layout

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,14 @@
+class FriendshipsController < ApplicationController
+    def create
+        user = User.find(params[:followed_id])
+        current_user.follow(user)
+        redirect_to(:back)   
+    end
+    
+    def destroy
+        user = Friendship.find(params[:id]).followed
+        current_user.unfollow(user)
+        redirect_to(:back)   
+    end
+    
+end
