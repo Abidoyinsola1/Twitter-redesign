cd403b244799aea7ccd9323df49887e7bb4f03e4
Abiola Doyinsola <ifeobayemi@gmail.com>
2020-11-20 13:50:05 +0100

    Clean up code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,5 @@ module ApplicationHelper
     def suggest
         @user = User.all - [current_user]
         render 'follow-suggest'
-    end
-    def account
-        
-    end
-    
+    end    
 end
--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,14 +1,28 @@
 module TweetsHelper
     def follow_or_unfollow_button(user)
-
-        out = ''
+        if user_signed_in?
+           out = ''
         if current_user.following?(user)
-          out << link_to('unfollow', followers_user_path(id: user.id), class: 'profile-link f-link')
+          out << link_to('unfollow', followers_user_path(id: user.id), class: '')
         else
-          out << link_to('follow', following_user_path(id: user.id), class: 'profile-link f-link')
+          out << link_to('follow', following_user_path(id: user.id), class: '')
         end
     
         out.html_safe
+    end
+    end
+
+    def navigations
+        output = ''
+        if user_signed_in?
+            output << link_to('Edit Profile', edit_user_registration_path, class: 'button is-info')
+            output << link_to('Log Out', destroy_user_session_path, class: 'button is-info', method: :delete )
+        else
+            output << link_to('Sign In', new_user_session_path, class: 'button is-info' )                       
+            output << link_to('Sign Up', new_user_registration_path, class: 'button is-info' )
+
+        end
+            output.html_safe
     end
     
 end
