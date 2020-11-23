eaf5b63c0004bc6362e05a1fe2f8aef119a8f9fb
Abiola Doyinsola <ifeobayemi@gmail.com>
2020-11-23 07:52:28 +0100

    Add friendship test

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :image)
   end
 end
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
- 
+    context 'positive validation test' do
+      it 'ensures presence of follower id' do
+        User.new(name: 'Doyinsola', email: 'doyin@gmail.com', password: 'doyinsola', id: 1).save
+        friend = Friendship.new( follower_id: 2).save
+        expect(friend).to eq(false)
+      end
+    end
+    context 'negative validation test' do
+      it 'ensures presence of user id and friend id' do
+        User.new(name: 'Ayomide', email: 'ayomide@gmail.com', password: 'ayomide', id: 1).save
+        User.new(name: 'Doyin', email: 'doyin@gmail.com', password: 'doyinreal', id: 2).save
+        friend = Friendship.new(user_id: 1).save
+        expect(friend).to eq(false)
+      end
+    end
+    context 'negative validation test' do
+      it 'ensures presence of user id and friend id' do
+        User.new(name: 'Ayomide', email: 'ayomide@gmail.com', password: 'ayomide', id: 1).save
+        User.new(name: 'Doyin', email: 'doyin@gmail.com', password: 'doyinreal', id: 2).save
+        friend = Friendship.new(friend_id: 2).save
+        expect(friend).to eq(false)
+      end
+    end
+  
 end
