9a0d6d595d11f02e7260d9fa596bcb36d7f86a51
Abiola Doyinsola <ifeobayemi@gmail.com>
2020-11-30 16:51:37 +0100

    Add friendship controller test

---
+++ spec/requests/friendship_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Friendships", type: :request do
+  describe "friendships" do
+    context "when user is not a friend" do
+      it 'should create friendship' do
+      end
+    end
+    
+    context "when a user is a friend" do
+      it 'should delete the friendship' do
+        
+      end
+    end
+    
+  end
+end
