43e0859563f4641c48a4dc0be941b9eff17da99d
Abiola Doyinsola <ifeobayemi@gmail.com>
2020-12-02 09:33:50 +0100

    Review tweet helper

--- spec/helpers/tweet_helper_spec.rb
+++ spec/helpers/tweet_helper_spec.rb
@@ -1,34 +1,13 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the TweetHelper. For example:
-#
-# describe TweetHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe TweetsHelper, type: :helper do
-  context 'when user is signed in' do
-    it 'shows follow button' do
-    end
-    it 'shows unfollow button' do
-    end
-  end
+  include Devise::Test::ControllerHelpers
 
-  context 'when user is signed in' do
-    it 'shows Edit Profile' do
-    end
-    it 'shows Log Out' do
-    end
-  end
-
-  context 'when user is logged out' do
-    it 'shows Sign In' do
-    end
-    it 'shows Sign Up' do
+  describe TweetsHelper do
+    describe 'follow_or_unfollow_button' do
+      it 'is following' do
+        expect(helper.follow_or_unfollow_button('Abiola')).to eq(nil)
+      end
     end
   end
 end
